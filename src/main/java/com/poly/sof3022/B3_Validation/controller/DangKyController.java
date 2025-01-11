7b1b6f87721c490d409e55e60bcc5185c9c3cafa
HangNT169 <HangNT169@fpt.edu.vn>
2025-01-11 11:18:16 +0700

    Buổi 3: Spring validation

---
+++ src/main/java/com/poly/sof3022/B3_Validation/controller/DangKyController.java
@@ -0,0 +1,30 @@
+package com.poly.sof3022.B3_Validation.controller;
+
+import com.poly.sof3022.B3_Validation.request.DangKyRequest;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class DangKyController {
+    @GetMapping("dang-ky")
+    public String hienThiFormDangKy(Model model) {
+        // Tao ra 1 doi tuong de view hung de validate cac truong tren doi tuong day
+        model.addAttribute("request1", new DangKyRequest());
+        return "buoi3/dang-ky";
+    }
+
+    @PostMapping("ket-qua1")
+    public String xuLyDangKy(@Valid @ModelAttribute("request1") DangKyRequest request,
+                             BindingResult result, Model model) {
+        if(result.hasErrors()){
+            return "buoi3/dang-ky";
+        }
+        model.addAttribute("req", request);
+        return "buoi3/ket-qua";
+    }
+}
---
+++ src/main/java/com/poly/sof3022/B3_Validation/request/DangKyRequest.java
@@ -0,0 +1,28 @@
+package com.poly.sof3022.B3_Validation.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DangKyRequest {
+    @NotBlank
+    private String fullName;
+
+    @NotEmpty(message = "Password khong duoc de trong")
+//    @Pattern(regexp = "doan regex mong muon",message = "custom mess")
+//    @Min(5)
+//    @Max(10)
+//    @Size
+    private String passWord;
+
+    private Boolean gender;
+
+
+}
